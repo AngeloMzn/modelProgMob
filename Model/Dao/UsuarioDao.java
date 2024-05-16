b9c85c46236557f978c8a3e5e870ac7ac57aec34
Ângelo Vinicius Hernandez Mazarin <106436644+AngeloMzn@users.noreply.github.com>
2024-05-16 15:52:24 -0400

    Update UsuarioDao.java

--- Model/Dao/UsuarioDao.java
+++ Model/Dao/UsuarioDao.java
@@ -11,19 +11,19 @@ import java.util.List;
 
 @Dao
 public interface UsuarioDao {
-    @Query("SELECT * FROM usuario WHERE id=:idUsu LIMIT 1")
-    Usuario getUser(int idUsu);
+    @Query("SELECT * FROM Usuario WHERE id=:idUser LIMIT 1")
+    Usuario getUser(int idUser);
 
     @Query("SELECT * FROM Usuario")
     List<Usuario> getAll();
 
     @Query("SELECT * FROM usuario WHERE login like :userLogin")
     Usuario getUserByLogin(String userLogin);
-    
+
     @Insert
-    void insertAll(Usuario usuario);
+    void insertAll(Usuario user);
     @Update
-    void update(Usuario usuario);
+    void update(Usuario user);
     @Delete
-    void delete(Usuario usuario);
+    void delete(Usuario user);
 }
